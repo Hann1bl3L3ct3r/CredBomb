1e3bc911830c37eb647c96f5e6cd334b4b088c36
Hann1bl3L3ct3r <141247795+Hann1bl3L3ct3r@users.noreply.github.com>
2025-04-14 18:52:51 -0400

    Update scanner.py
    New progress bar.

--- scanner.py
+++ scanner.py
@@ -12,6 +12,7 @@ from modules.postgresql import check_postgresql
 from modules.redis import check_redis
 from modules.vnc import check_vnc
 from rich import print
+from rich.progress import Progress
 import json
 from datetime import datetime
 import os
@@ -25,56 +26,64 @@ def main():
     targets = scan_subnet(args.subnet)
 
     results = []
-    for host, ports in targets.items():
-        host_result = {"ip": host, "vulnerabilities": []}
-        if 445 in ports:
-            smb_vuln = check_smb(host)
-            if smb_vuln:
-                host_result["vulnerabilities"].append(smb_vuln)
-        if 21 in ports:
-            ftp_vuln = check_ftp(host)
-            if ftp_vuln:
-                host_result["vulnerabilities"].append(ftp_vuln)
-        if 22 in ports:
-            ssh_vuln = check_ssh(host)
-            if ssh_vuln:
-                host_result["vulnerabilities"].append(ssh_vuln)
-        if 23 in ports:
-            telnet_vuln = check_telnet(host)
-            if telnet_vuln:
-                host_result["vulnerabilities"].append(telnet_vuln)
-        if 161 in ports:
-            snmp_vuln = check_snmp(host)
-            if snmp_vuln:
-                host_result["vulnerabilities"].append(snmp_vuln)
-        http_ports = [80, 8080, 8000, 8888, 443, 8443]
-        open_http_ports = list(set(http_ports) & set(ports))
-        if open_http_ports:
-            http_vuln = check_http(host, open_http_ports)
-            if http_vuln:
-                host_result["vulnerabilities"].append(http_vuln)
-        if 389 in ports:
-            ldap_vuln = check_ldap(host)
-            if ldap_vuln:
-                host_result["vulnerabilities"].append(ldap_vuln)
-        if 3306 in ports:
-            mysql_vuln = check_mysql(host)
-            if mysql_vuln:
-                host_result["vulnerabilities"].append(mysql_vuln)
-        if 5432 in ports:
-            pg_vuln = check_postgresql(host)
-            if pg_vuln:
-                host_result["vulnerabilities"].append(pg_vuln)
-        if 6379 in ports:
-            redis_vuln = check_redis(host)
-            if redis_vuln:
-                host_result["vulnerabilities"].append(redis_vuln)
-         if 5900 in ports:
-            vnc_vuln = check_vnc(host)
-            if vnc_vuln:
-                host_result["vulnerabilities"].append(vnc_vuln)
-        if host_result["vulnerabilities"]:
-            results.append(host_result)
+    with Progress() as progress:
+        task = progress.add_task("[green]Scanning hosts...", total=len(targets))
+
+        for host, ports in targets.items():
+            host_result = {"ip": host, "vulnerabilities": []}
+
+            if 445 in ports:
+                smb_vuln = check_smb(host)
+                if smb_vuln:
+                    host_result["vulnerabilities"].append(smb_vuln)
+            if 21 in ports:
+                ftp_vuln = check_ftp(host)
+                if ftp_vuln:
+                    host_result["vulnerabilities"].append(ftp_vuln)
+            if 22 in ports:
+                ssh_vuln = check_ssh(host)
+                if ssh_vuln:
+                    host_result["vulnerabilities"].append(ssh_vuln)
+            if 23 in ports:
+                telnet_vuln = check_telnet(host)
+                if telnet_vuln:
+                    host_result["vulnerabilities"].append(telnet_vuln)
+            if 161 in ports:
+                snmp_vuln = check_snmp(host)
+                if snmp_vuln:
+                    host_result["vulnerabilities"].append(snmp_vuln)
+            http_ports = [80, 8080, 8000, 8888, 443, 8443]
+            open_http_ports = list(set(http_ports) & set(ports))
+            if open_http_ports:
+                http_vuln = check_http(host, open_http_ports)
+                if http_vuln:
+                    host_result["vulnerabilities"].append(http_vuln)
+            if 389 in ports:
+                ldap_vuln = check_ldap(host)
+                if ldap_vuln:
+                    host_result["vulnerabilities"].append(ldap_vuln)
+            if 3306 in ports:
+                mysql_vuln = check_mysql(host)
+                if mysql_vuln:
+                    host_result["vulnerabilities"].append(mysql_vuln)
+            if 5432 in ports:
+                pg_vuln = check_postgresql(host)
+                if pg_vuln:
+                    host_result["vulnerabilities"].append(pg_vuln)
+            if 6379 in ports:
+                redis_vuln = check_redis(host)
+                if redis_vuln:
+                    host_result["vulnerabilities"].append(redis_vuln)
+            if 5900 in ports:
+                vnc_vuln = check_vnc(host)
+                if vnc_vuln:
+                    host_result["vulnerabilities"].append(vnc_vuln)
+
+            if host_result["vulnerabilities"]:
+                results.append(host_result)
+
+            progress.update(task, advance=1)
+
     if not os.path.exists("reports"):
         os.makedirs("reports")
 
